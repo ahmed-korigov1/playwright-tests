fb239a3625cfa17dfe5b27f5bbd52a62956e34f5
ahmed-korigov1 <korigov-akhmed@mail.ru>
2026-02-02 18:20:35 +0300

    move login locators to separate module

---
+++ locators/login_locators.py
@@ -0,0 +1,3 @@
+USERNAME_INPUT = "#user-name"
+PASSWORD_INPUT = "#password"
+LOGIN_BUTTON = "#login-button"
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,19 @@
+from locators.login_locators import (
+    USERNAME_INPUT,
+    PASSWORD_INPUT,
+    LOGIN_BUTTON,
+)
+
+
 class LoginPage:
 
     def __init__(self, page):
         self.page = page
-        self.username_input = "#user-name"
-        self.password_input = "#password"
-        self.login_button = "#login-button"
 
     def open(self):
         self.page.goto(self.page.base_url)
 
     def login(self, user, password):
-        self.page.fill(self.username_input, user)
-        self.page.fill(self.password_input, password)
-        self.page.click(self.login_button)
+        self.page.fill(USERNAME_INPUT, user)
+        self.page.fill(PASSWORD_INPUT, password)
+        self.page.click(LOGIN_BUTTON)
